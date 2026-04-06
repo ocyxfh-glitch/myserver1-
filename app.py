5ed492087b605b5b75e53d79a83d8fbd45c6173a
Peter <ocyxfh@gmail.com>
2026-04-06 13:11:09 +0530

    added file sharing

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Server Running"
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory("uploads", filename, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)from flask import Flask, render_template
 
 app = Flask(__name__)
 
