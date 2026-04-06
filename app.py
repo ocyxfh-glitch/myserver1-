c7d29b752bfda8070ba26b90826d5d6a1ce04c91
Peter <ocyxfh@gmail.com>
2026-04-06 13:01:35 +0530

    added dashboard UI

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)from flask import Flask
 
 app = Flask(__name__)
 
